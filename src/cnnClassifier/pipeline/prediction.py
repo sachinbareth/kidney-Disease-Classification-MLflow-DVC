2151ae71c02f4ebebb36dca6a823595039acf8df
Sachin <sachinbareth099@gmail.com>
2024-04-07 16:10:54 +0530

    prediction added

--- src/cnnClassifier/pipeline/prediction.py
+++ src/cnnClassifier/pipeline/prediction.py
@@ -1,33 +1,30 @@
 import numpy as np
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
 import os
 
-try:
-    from tensorflow.keras.models import load_model
-    from tensorflow.keras.preprocessing import image
-except ImportError as e:
-    error_message = str(e)
+
 
 class PredictionPipeline:
-    class_labels = ['Cyst', 'Normal', 'Stone', 'Tumor']
+    def __init__(self,filename):
+        self.filename =filename
 
-    def __init__(self, filename):
-        self.filename = filename
+
     
     def predict(self):
-        if 'error_message' in locals():
-            return {"error": error_message}
-        
         # load model
         model = load_model(os.path.join("model", "model.h5"))
 
         imagename = self.filename
-        test_image = image.load_img(imagename, target_size=(224, 224))
+        test_image = image.load_img(imagename, target_size = (224,224))
         test_image = image.img_to_array(test_image)
-        test_image = np.expand_dims(test_image, axis=0)
-        
-        # Make prediction
-        predictions = model.predict(test_image)
-        result_class = self.class_labels[np.argmax(predictions)]
-        # print("Predicted class:", result_class)
+        test_image = np.expand_dims(test_image, axis = 0)
+        result = np.argmax(model.predict(test_image), axis=1)
+        print(result)
 
-        return {"image": result_class}
+        if result[0] == 1:
+            prediction = 'Tumor'
+            return [{ "image" : prediction}]
+        else:
+            prediction = 'Normal'
+            return [{ "image" : prediction}]
